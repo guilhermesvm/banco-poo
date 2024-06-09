e05739f4db951dc729e06e389bb134b6a2562f9d
Murilo Klein <murilo.kkleinn@gmail.com>
2024-06-09 19:40:56 -0300

    Adicionado index

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// Exemplo de uso
+const banco = new Banco("Banco Exemplo");
+const cliente1 = new Cliente("Fulano", "123.456.789-00");
+const contaCorrente = new ContaCorrente("12345", 1000, 500);
+const contaPoupanca = new ContaPoupanca("67890", 2000, 0.5);
+
+cliente1.adicionarConta(contaCorrente);
+cliente1.adicionarConta(contaPoupanca);
+banco.adicionarCliente(cliente1);
+
+contaCorrente.depositar(200);
+contaCorrente.sacar(150);
+contaCorrente.usarChequeEspecial(200);
+
+contaPoupanca.aplicarJuros();
+
+console.log(banco);
