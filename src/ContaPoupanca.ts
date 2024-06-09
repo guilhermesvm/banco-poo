95625b49e40a7d7e1e1864f3bbdeb3160161b827
Murilo Klein <murilo.kkleinn@gmail.com>
2024-06-09 19:40:06 -0300

    Adicionado classe de Conta Poupanca

---
+++ src/ContaPoupanca.ts
@@ -0,0 +1,14 @@
+class ContaPoupanca extends Conta {
+  taxaJuros: number;
+
+  constructor(numero: string, saldo: number, taxaJuros: number) {
+    super(numero, saldo);
+    this.taxaJuros = taxaJuros;
+  }
+
+  aplicarJuros(): void {
+    const juros = (this.saldo * this.taxaJuros) / 100;
+    this.saldo += juros;
+    this.transacoes.push(new Transacao(juros, new Date()));
+  }
+}
