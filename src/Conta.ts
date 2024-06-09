9dee313440669356378dba4e4da60c38bedfd7c5
Murilo Klein <murilo.kkleinn@gmail.com>
2024-06-09 19:38:44 -0300

    Adicionado classe de Conta

---
+++ src/Conta.ts
@@ -0,0 +1,25 @@
+abstract class Conta {
+  numero: string;
+  saldo: number;
+  transacoes: Transacao[];
+
+  constructor(numero: string, saldo: number) {
+    this.numero = numero;
+    this.saldo = saldo;
+    this.transacoes = [];
+  }
+
+  depositar(valor: number): void {
+    this.saldo += valor;
+    this.transacoes.push(new Transacao(valor, new Date()));
+  }
+
+  sacar(valor: number): void {
+    if (valor <= this.saldo) {
+      this.saldo -= valor;
+      this.transacoes.push(new Transacao(-valor, new Date()));
+    } else {
+      throw new Error("Saldo insuficiente.");
+    }
+  }
+}
